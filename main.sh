6202a2d0c23b03a2e4e25f4e716e8a30367a5f01
emkor93 <emkor93@gmail.com>
2018-04-20 19:44:10 +0200

    added next few commands to resolve os dependencies and install packstack

--- main.sh
+++ main.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
 yum install -y sudo wget curl nano vim git htop
+
+sudo systemctl disable firewalld
+sudo systemctl stop firewalld
+sudo systemctl disable NetworkManager
+sudo systemctl stop NetworkManager
+sudo systemctl enable network
+sudo systemctl start network
+
+sudo yum install -y centos-release-openstack-queens
+sudo yum update -y
+sudo yum install -y openstack-packstack
