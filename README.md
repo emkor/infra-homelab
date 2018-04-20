5c572a5415b2033e91ae2272e4f4ed8b40a5d904
emkor93 <emkor93@gmail.com>
2018-04-20 20:00:47 +0200

    added network configuration and started readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# openstack-on-hp-z600
+# Example: Installing OpenStack with Packstack within, local existing network (homelab purposes, HP Z600)
+
+### Prerequisites
+- CentOS 7
+- Specific network setup, so it's possible to use few IP addresses for your OpenStack VMs
+
+### Usage:
+- ssh to your machine
+- make sure you're root or user with sudo
+- `yum install git`
+- `git clone <THIS REPO URL>`
+- `cd openstack-on-hp-z600`
+- `chmod u+x ./main.sh`
+- `./main.sh`
+- be patient!
--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-yum install -y sudo wget curl nano vim git htop
+yum install -y sudo wget curl nano vim htop
 
 sudo systemctl disable firewalld
 sudo systemctl stop firewalld
@@ -12,3 +12,21 @@ sudo systemctl start network
 sudo yum install -y centos-release-openstack-queens
 sudo yum update -y
 sudo yum install -y openstack-packstack
+
+packstack --allinone --provision-demo=n --os-neutron-ovs-bridge-mappings=extnet:br-ex --os-neutron-ovs-bridge-interfaces=br-ex:enp1s0 --os-neutron-ml2-type-drivers=vxlan,flat
+
+sudo cp files/ifcfg-br-ex /etc/sysconfig/network-scripts/ifcfg-br-ex
+sudo cp files/ifcfg-enp1s0 /etc/sysconfig/network-scripts/ifcfg-enp1s0
+systemctl restart network
+
+source keystonerc_admin
+neutron net-create external_network --provider:network_type flat --provider:physical_network extnet  --router:external
+neutron subnet-create --name public_subnet --enable_dhcp=False --allocation-pool=start=192.168.193.1,end=192.168.193.250 \
+                        --gateway=192.168.192.254 external_network 192.168.192.0/23
+
+neutron router-create main_router
+neutron router-gateway-set main_router external_network
+
+neutron net-create private_network
+neutron subnet-create --name private_subnet private_network 172.16.0.0/16
+neutron router-interface-add main_router private_subnet
