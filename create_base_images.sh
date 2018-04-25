57f0877539b0ea2a2864b941ced1ea003ae0601c
emkor93 <emkor93@gmail.com>
2018-04-25 21:12:31 +0200

    added coreos image upload

--- create_base_images.sh
+++ create_base_images.sh
@@ -9,3 +9,8 @@ mkdir ~/image && cd ~/image
 wget http://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2 -O CentOS-7.qcow2
 glance image-create --name CentOS-7 --architecture x86_64 --disk-format qcow2 --container-format bare --visibility public --protected True --file ./CentOS-7.qcow2
 rm -f ./CentOS-7.qcow2
+
+wget https://stable.release.core-os.net/amd64-usr/current/coreos_production_openstack_image.img.bz2
+bunzip2 coreos_production_openstack_image.img.bz2
+glance image-create --name CoreOS --disk-format qcow2 --container-format bare --visibility public --protected True --file ./coreos_production_openstack_image.img
+rm -f coreos_production_openstack_image.img
