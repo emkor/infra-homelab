2404aa4b74265ac052a47125d5be46799f92e433
emkor93 <emkor93@gmail.com>
2018-04-26 19:24:03 +0200

    moved creating project and user to separate step

--- configure.sh
+++ configure.sh
@@ -31,11 +31,6 @@ sed -i "s/ram_allocation_ratio.*=.*/ram_allocation_ratio=1.0/g" /etc/nova/nova.c
 # create development project, add guest user, update project quotas, disable admin project
 cd ~
 source ~/keystonerc_admin
-openstack project create --description 'development' development --domain default --enable
-openstack role add --user admin --project development admin
-openstack quota set --instances 40 --key-pairs 20 --floating-ips 40 --cores 40 --ram 40960 --gigabytes 400 --volumes 20 --per-volume-gigabytes 40 --snapshots 10 development
-openstack user create --project development --password guest guest
-openstack role add --user guest --project development _member_
 
 openstack service delete aodh
 openstack service delete gnocchi
--- post_install.sh
+++ post_install.sh
@@ -13,9 +13,17 @@ sed -i "s/#dnsmasq_dns_servers =/dnsmasq_dns_servers = 1.1.1.1, 8.8.8.8, 8.8.4.4
 
 cd ~
 source ~/keystonerc_admin
-neutron net-create external_network --provider:network_type flat --provider:physical_network extnet  --router:external
+neutron net-create external_network --provider:network_type flat --provider:physical_network extnet  --router:external --shared
 neutron subnet-create --name public_subnet --enable_dhcp=False --allocation-pool=start=192.168.193.1,end=192.168.193.250 \
                         --gateway=192.168.192.254 external_network 192.168.192.0/23
+
+openstack project create --enable development
+openstack role add --user admin --project development admin
+openstack user create --project development --password guest --enable guest
+openstack role add --user guest --project development _member_
+openstack quota set --instances 40 --key-pairs 20 --floating-ips 40 --cores 40 --ram 40960 --gigabytes 400 --volumes 20 --per-volume-gigabytes 40 --snapshots 10 development
+
+export OS_TENANT_NAME=development
 
 neutron router-create main_router
 neutron router-gateway-set main_router external_network
