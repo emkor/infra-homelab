a950a28c6e08fa9fc70de5e63475761d06415575
emkor93 <emkor93@gmail.com>
2018-04-26 19:47:32 +0200

    switched to installation using answers file

--- install.sh
+++ install.sh
@@ -8,7 +8,4 @@ sudo yum install -y centos-release-openstack-queens
 sudo yum update -y
 sudo yum install -y openstack-packstack
 
-packstack --allinone --provision-demo=n --os-neutron-ovs-bridge-mappings=extnet:br-ex --os-neutron-ovs-bridge-interfaces=br-ex:enp1s0 --os-neutron-ml2-type-drivers=vxlan,flat
-
-
-
+packstack --answer-file=packstack-answers.txt
