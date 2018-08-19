4745de31e17ad9d381ebd7bebee83db21d0c4a0b
emkor93 <emkor93@gmail.com>
2018-08-19 20:21:16 +0200

    simplified packstack command

--- install.sh
+++ install.sh
@@ -2,10 +2,8 @@
 
 set -e
 
-cd ~/openstack-on-hp-z600
-
 yum install -y centos-release-openstack-queens
 yum update -y
 yum install -y openstack-packstack
 
-packstack --answer-file=files/packstack-answers.txt
+packstack --answer-file=~/openstack-on-hp-z600/files/packstack-answers.txt
