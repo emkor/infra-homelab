6a9fc6b47d5a30c09961fdb6832436b279578269
emkor93 <emkor93@gmail.com>
2018-10-19 20:49:41 +0200

    updated script for image with python 3.6 installed, updated commentary to cert generation script

--- image/ubuntu_1804_docker_reg/generate_certs.sh
+++ image/ubuntu_1804_docker_reg/generate_certs.sh
@@ -4,6 +4,5 @@ set -e
 
 echo "Generating self-signed certificates for Docker Registry"
 mkdir -p ~/certs
-
-# as FQDN, type: *.cloud.local
+# as Common Name / CN, type: *.cloud.local
 openssl req -newkey rsa:4096 -nodes -sha256 -keyout ~/certs/domain.key -x509 -days 1460 -out ~/certs/ca.crt
--- image/ubuntu_1804_py3/update.sh
+++ image/ubuntu_1804_py3/update.sh
@@ -2,26 +2,22 @@
 
 set -e
 
-echo 'Installing Python 3 development server...'
-
 sudo apt-get update
-sudo apt-get install -y python3-venv python3-pip
+sudo apt-get install -y curl make
+sudo apt-get install -y python3-venv python3-distutils
 
-echo "alias python=python3" >> ~/.bashrc
-echo "alias pip=pip3" >> ~/.bashrc
-source ~/.bashrc
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+sudo python3 get-pip.py
 
-pip install wheel
+sudo apt clean -y
+sudo apt autoclean -y
+sudo apt autoremove -y
+
+echo 'Done!'
+history -c
 
 # for testing if OS deps are available:
 # python3 -m venv ~/.venv
 # source ~/.venv/bin/activate
 # pip install wheel
 # pip install flask requests cryptography numpy scipy sqlalchemy Django cherrypy celery redis assertpy
-
-sudo apt clean -y
-sudo apt autoclean -y
-sudo apt autoremove -y
-history -c
-
-echo 'Done!'
