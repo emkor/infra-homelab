25c99ef82de03a8e75d8d131b9cb2480263ad7c7
emkor93 <emkor93@gmail.com>
2019-09-04 09:18:43 +0200

    add venv check

--- image/ubuntu_1804_py36/update.sh
+++ image/ubuntu_1804_py36/update.sh
@@ -6,6 +6,7 @@ sudo systemctl mask apt-daily.service apt-daily-upgrade.service
 sudo apt-get update
 sudo apt-get install -y curl make zip
 sudo apt-get install -y python3-venv python3-distutils python3-setuptools python3-wheel python3-pip
+sudo ln -s /usr/bin/python3 /usr/bin/python
 
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 sudo python3 get-pip.py
@@ -15,15 +16,8 @@ sudo apt clean -y
 sudo apt autoclean -y
 sudo apt autoremove -y
 
-sudo ln -s /usr/bin/python3 /usr/bin/python
-
+python -m venv --help # testing if venv module is available
 python --version
 pip --version
-echo 'Done!'
-history -c
 
-# for testing if OS deps are available:
-# python3 -m venv ~/.venv
-# source ~/.venv/bin/activate
-# pip install wheel
-# pip install flask requests cryptography numpy scipy sqlalchemy Django cherrypy celery redis assertpy
+history -c
