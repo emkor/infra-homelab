076b7710cbbb5cfc0a2a7767ea477f130f426872
emkor93 <emkor93@gmail.com>
2019-11-26 20:45:47 +0100

    add setup for both images - 3.7 and 3.8

--- image/ubuntu_1804_py37/update.sh
+++ image/ubuntu_1804_py37/update.sh
@@ -4,10 +4,10 @@ set -e
 
 sudo apt update
 sudo apt-get install -y curl make zip software-properties-common
-sudo apt install -y python3.8 python3.8-venv python3-distutils
+sudo apt install -y python3.7 python3.7-venv python3-distutils
 sudo apt install -y python3-distutils
 
-sudo ln -s /usr/bin/python3.8 /usr/bin/python
+sudo ln -s /usr/bin/python3.7 /usr/bin/python
 
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 sudo python get-pip.py
