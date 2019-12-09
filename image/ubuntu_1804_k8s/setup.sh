9555657116184a78e9a2e60a62249136e74cac01
emkor93 <emkor93@gmail.com>
2019-12-09 20:36:51 +0100

    updated setup script for ubuntu k8s

--- image/ubuntu_1804_k8s/setup.sh
+++ image/ubuntu_1804_k8s/setup.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-PRIVATE_NETWORK_UUID="dd903afe-d743-4256-90b4-330b4d9d03d4"
-EXTERNAL_NETWORK_UUID="07e092c6-482e-4889-96e9-4cfe97b9be2e"
-UBUNTU_IMAGE_UUID="ec561efe-7ece-48a0-91f5-edbf04f9f04a"
+PRIVATE_NETWORK_UUID="5b21d621-448b-4071-a6c6-95b07afe118d"
+EXTERNAL_NETWORK_UUID="48e05747-f3f7-4b03-a7a5-5e91ad31ec8a"
+UBUNTU_IMAGE_UUID="95d3e694-dd18-4f4d-aeb2-7c297b69fe4f"
 
 sudo snap install juju --classic
 
@@ -12,7 +12,7 @@ juju add-cloud --local openstack-teczowa clouds.yaml
 juju add-credential openstack-teczowa -f creds.yaml
 
 mkdir -p ~/simplestreams/images
-juju metadata generate-image -d ~/simplestreams -i $UBUNTU_IMAGE_UUID -s bionic -r RegionOne -u http://192.168.192.250:5000/v3
+juju metadata generate-image -d ~/simplestreams -i $UBUNTU_IMAGE_UUID -r RegionOne -u http://192.168.192.250:5000/v3
 
 juju bootstrap openstack-teczowa juju-teczowa --config network=$PRIVATE_NETWORK_UUID --config external-network=$EXTERNAL_NETWORK_UUID --config use-floating-ip=true --metadata-source /home/ubuntu/simplestreams --credential guest --verbose
 
@@ -24,15 +24,15 @@ juju model-config use-floating-ip=true
 juju deploy charmed-kubernetes --verbose
 
 # to watch status:
-watch -c juju status --color
+#watch -c juju status --color
 
 # FROM https://ubuntu.com/kubernetes/docs/operations
 # wait till installation is done, and then:
-mkdir -p ~/.kube
-juju scp kubernetes-master/0:config ~/.kube/config
-kubectl config view
+#mkdir -p ~/.kube
+#juju scp kubernetes-master/0:config ~/.kube/config
+#kubectl config view
 
 # to access GUI, use:
-kubectl proxy
+#kubectl proxy
 
 # then, navigate to: http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/
