52beca1603b8086696945650a6459e21b0e78eda
emkor93 <emkor93@gmail.com>
2020-01-13 10:32:36 +0100

    update ubuntu 18.04 lts docker image recipe

--- image/ubuntu_1804_docker/update.sh
+++ image/ubuntu_1804_docker/update.sh
@@ -2,25 +2,10 @@
 
 set -e
 
-# disable annoying apt-daily-upgrade.service
-sudo systemctl mask apt-daily.service apt-daily-upgrade.service
-sudo systemctl stop apt-daily.service
-sudo systemctl kill --kill-who=all apt-daily.service
-# wait until `apt-get updated` has been killed
-while ! (sudo systemctl list-units --all apt-daily.service | fgrep -q dead)
-do
-  sleep 1;
-done
-
 # pre install
 sudo apt-get update
 sudo apt-get install -y curl make zip
-sudo apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
@@ -31,16 +16,16 @@ sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # post-installation steps
-sudo groupadd docker
+sudo groupadd --force docker
 sudo usermod -aG docker $USER
 sudo systemctl enable docker
 
 # install docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # install ctop
-sudo wget https://github.com/bcicen/ctop/releases/download/v0.7.2/ctop-0.7.2-linux-amd64 -O /usr/local/bin/ctop
+sudo wget https://github.com/bcicen/ctop/releases/download/v0.7.3/ctop-0.7.3-linux-amd64 -O /usr/local/bin/ctop
 sudo chmod +x /usr/local/bin/ctop
 
 sudo systemctl restart docker
