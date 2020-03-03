4796de896e2c9bd0fc62b45a721a602e64a25eb0
emkor93 <emkor93@gmail.com>
2020-03-03 09:58:05 +0100

    change the location of generated certs

--- image/ubuntu_1804_docker_reg/generate_certs.sh
+++ image/ubuntu_1804_docker_reg/generate_certs.sh
@@ -3,6 +3,6 @@
 set -e
 
 echo "Generating self-signed certificates for Docker Registry"
-mkdir -p ~/certs
+mkdir -p ~/cert
 # as Common Name / CN, type: *.cloud.local
-openssl req -newkey rsa:4096 -nodes -sha256 -keyout ~/certs/domain.key -x509 -days 1460 -out ~/certs/ca.crt
+openssl req -newkey rsa:4096 -nodes -sha256 -keyout ~/cert/domain.key -x509 -days 1460 -out ~/cert/ca.crt
