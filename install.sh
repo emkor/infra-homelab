09855a1dec29091d760a95ad27c32f6706865f1c
emkor93 <emkor93@gmail.com>
2020-04-12 20:35:34 +0200

    update answers.cfg for the new packstack version

--- install.sh
+++ install.sh
@@ -2,10 +2,13 @@
 
 set -e
 
-yum install -y https://rdoproject.org/repos/rdo-release.rpm
 yum install -y centos-release-openstack-stein
 
 yum update -y
-yum install -y openstack-packstack
 
-packstack --answer-file=packstack-answers.txt --debug
+# workaround: puppet binaries look for leatherman_curl.so.1.3.0
+yum erase leatherman-1.10.0-1.el7
+yum install -y leatherman-1.3.0-9.el7
+
+yum install -y openstack-packstack
+packstack --answer-file=answers.cfg --debug
